6772b105268a339ffa16205a3910c8e2562fb88a
Anton Istomin <istomanton@gmail.com>
2023-02-12 18:07:33 +0400

    First working verison

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
     dockerSocketPath: process.env.DOCKER_SOCKET_PATH ?? '/var/run/docker.sock',
+    telegramBotToken: process.env.TELEGRAM_BOT_TOKEN ?? '12345',
+    telegramChatId: process.env.TELEGRAM_CHAT_ID ?? '12345',
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
-import Docker from 'dockerode'
+import Docker, { ContainerInfo } from 'dockerode';
 import config from './config';
+import TelegramBot from 'node-telegram-bot-api';
 
 const docker = new Docker({ socketPath: config.dockerSocketPath });
+const telegramBot = new TelegramBot(config.telegramBotToken);
 
-async function info() {
+async function listenLogs() {
     const containers = await docker.listContainers();
-    console.log(JSON.stringify(containers, null, 2));
+
+    const logPromises = containers.map((containerInfo: ContainerInfo) => {
+        const container = docker.getContainer(containerInfo.Id);
+        return container.logs({
+            timestamps: true,
+            stdout: true,
+            stderr: true,
+            tail: 0,
+            follow: true,
+        });
+    });
+
+    const logStreams = await Promise.all(logPromises);
+
+    logStreams.forEach((stream, index) => {
+        stream.on('data', (buffer: Buffer) => {
+            const log = buffer.toString();
+
+            if (log.toLowerCase().indexOf('error') !== -1) {
+                const serviceName = containers[index].Image
+                telegramBot.sendMessage(
+                    config.telegramChatId,
+                    `*Error in service ${serviceName}*`,
+                    { parse_mode: 'Markdown' }
+                ).then(() => telegramBot.sendMessage(config.telegramChatId, log)).catch(console.error);
+            }
+        });
+    });
 }
 
-info().catch(console.error);
+async function run() {
+    await listenLogs();
+}
+
+run().catch(console.error);
